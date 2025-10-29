f419a4b6aa8642424cbb444390d25d6274de20f0
Deadlyzz021006 <khoirullohmuhammadr@gmail.com>
2025-10-29 21:12:15 +0700

    fixed email feedback function

---
+++ src/components/function/email.js
@@ -0,0 +1,40 @@
+import emailjs from "@emailjs/browser";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("feedbackForm");
+  const status = document.getElementById("feedbackStatus");
+
+  if (!form) return;
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    status.textContent = "Mengirim...";
+    status.classList.remove("text-green-400", "text-red-400");
+    status.classList.add("text-gray-400");
+
+    emailjs
+      .sendForm(
+        "service_4vrws49",   // SERVICE ID
+        "template_j7kzkh5",  // TEMPLATE ID
+        this,
+        "JYhlTDDIMyePEPshR"  // PUBLIC KEY
+      )
+
+      //MESSAGE STATUS
+      .then(
+        () => {
+          status.textContent = "✅ Feedback berhasil dikirim!";
+          status.classList.remove("text-gray-400");
+          status.classList.add("text-green-400");
+          form.reset();
+        },
+        (error) => {
+          console.error("EmailJS Error:", error);
+          status.textContent = "❌ Gagal mengirim. Coba lagi.";
+          status.classList.remove("text-gray-400");
+          status.classList.add("text-red-400");
+        }
+      );
+  });
+});
